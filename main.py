e3275cab648ff0f16641ab500400bf3db8ed93da
panzehir19 <41573243+panzehir19@users.noreply.github.com>
2020-12-12 15:53:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import json 
+import fileinput
+import wget
+import os
+
+if os.path.exists("list.json"):
+  os.remove("list.json")
+else:
+  print("Dosya mevcut değil, yeniden indiriliyor.")
+
+url = 'https://raw.githubusercontent.com/MISP/misp-warninglists/main/lists/vpn-ipv4/list.json'
+filename = wget.download(url)
+
+with open('list.json') as f:
+  data = json.load(f)
+
+  #print(data)
+
+#print(data['list'])
+#print(str(data))
+file = open('IP_Listesi.txt', 'w+')
+file.write(str(data["list"]))
+
+with open('IP_Listesi.txt', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace(',', '\n')
+filedata = filedata.replace('\'', ' ')
+filedata = filedata.replace('[', ' ')
+filedata = filedata.replace(']', ' ')
+
+
+with open('IP_Listesi.txt', 'w') as file:
+  file.write(filedata)
+
+
+file.close()
